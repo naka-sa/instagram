417706f2ce8d36772c5a5b0966553bd051d72d45
satoshi_nakajima <satoshi.nakajima@g.softbank.co.jp>
2017-03-23 11:07:07 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by satoshi_nakajima on 2017/03/23.
+//  Copyright © 2017年 satoshi_nakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
